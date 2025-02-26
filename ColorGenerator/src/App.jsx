336fcf54daffeb788a45891522290bc9c1603694
Sanjana <sanjana6122002@gmail.com>
2025-02-26 16:45:06 +0530

    colorgenerator

---
+++ ColorGenerator/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className='text-center font-bold text-4xl underline text-pink-800'>Color Generator Website</h1>
+    </>
+  )
+}
+
+export default App
